8eb36d9578847e00c5f739abeb8ca1fc238a9a3d
Robel Selemun <robel93gm@gmail.com>
2025-02-09 11:31:35 +0100

    test

--- src/types/ninja-order.ts
+++ src/types/ninja-order.ts
@@ -3,7 +3,7 @@ import type { NinjaOrderStatus } from '../schemas/ninja-orders';
 
 export interface NinjaOrder extends BaseEntity {
   service_type: string;
-  service_form_values: Record<string, unknown>;
+  service_form_values: Record<string, any>;
   company_id: string;
   status: NinjaOrderStatus;
   total_cost: number;
